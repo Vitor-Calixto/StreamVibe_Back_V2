4dc3dcb82cdaeab18322a2784b6e309ea55f259e
Vitor-Calixto <vitorpedrocalixto@gmail.com>
2025-07-01 18:28:55 -0300

    Delete _streamvibe_backend_java-main directory

--- _streamvibe_backend_java-main/_streamvibe_backend_java-main/src/main/java/com/streamvibe/api/models/Filme/FilmeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.streamvibe.api.models.filme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface filmeRepository extends JpaRepository<Usuario, Integer> {
-    
-}
